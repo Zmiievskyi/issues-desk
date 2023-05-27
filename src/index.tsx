89b135644a4cd1379fa3c343aebb20d46a987e25
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2023-05-27 22:59:35 +0100

    removed comment

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,6 @@ const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <div className="container">
@@ -20,7 +19,6 @@ root.render(
         </div>
       </Provider>
     </BrowserRouter>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
